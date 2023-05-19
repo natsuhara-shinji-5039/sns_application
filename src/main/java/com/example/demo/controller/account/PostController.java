0e0455d06803e8bbf42a952f1be16a06b02107d4
logical-natsu <natsuhara.shinji.5039@gmail.com>
2023-05-19 12:44:42 +0900

    コンフリクトの解消

--- src/main/java/com/example/demo/controller/account/PostController.java
+++ src/main/java/com/example/demo/controller/account/PostController.java
@@ -1,6 +1,13 @@
 package com.example.demo.controller.account;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
 	
 	@GetMapping("/posts/new")
+	public String create() {
+		return "account/posts/newPost";
+	}
 }
