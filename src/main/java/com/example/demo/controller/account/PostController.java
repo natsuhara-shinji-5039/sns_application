07c11c9f89d07769a113d27c1a153739a420fa80
logical-natsu <natsuhara.shinji.5039@gmail.com>
2023-05-19 13:33:05 +0900

    新規投稿機能の実装

--- src/main/java/com/example/demo/controller/account/PostController.java
+++ src/main/java/com/example/demo/controller/account/PostController.java
@@ -1,13 +1,41 @@
 package com.example.demo.controller.account;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Post;
+import com.example.demo.model.SessionAccount;
+import com.example.demo.repository.PostRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PostController {
 	
+	@Autowired
+	HttpSession session;
+	
+	@Autowired
+	SessionAccount sessionAccount;
+	
+	@Autowired
+	PostRepository postRepository;
+	
 	@GetMapping("/posts/new")
 	public String create() {
 		return "account/posts/newPost";
+	}
+	
+	@PostMapping("/posts")
+	public String store(
+			@RequestParam(name="body", defaultValue="") String body,
+			Model model) {
+		Post post = new Post(sessionAccount.getId(), 1, body);
+		postRepository.save(post);
+		return "redirect:/posts/new";
 	}
 }
