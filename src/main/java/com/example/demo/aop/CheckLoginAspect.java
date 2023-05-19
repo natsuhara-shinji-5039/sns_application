91dfb80c9607257076966ec86ff614253e2ef89b
logical-natsu <natsuhara.shinji.5039@gmail.com>
2023-05-19 15:58:58 +0900

    ログイン制限の実装

---
+++ src/main/java/com/example/demo/aop/CheckLoginAspect.java
@@ -0,0 +1,26 @@
+package com.example.demo.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.SessionAccount;
+
+@Aspect
+@Component
+public class CheckLoginAspect {
+	@Autowired
+	SessionAccount sessionAccount;
+	
+	// 
+	@Around("execution(* com.example.demo.controller.account.PostController.*(..))")
+	public Object checkLogin(ProceedingJoinPoint jp) throws Throwable {
+		if(sessionAccount == null || sessionAccount.getName() == null || sessionAccount.getName().length() == 0) {
+			System.err.println("ログインしていません");
+			return "redirect:/account/sign_in";
+		}
+		return jp.proceed();
+	}
+}
