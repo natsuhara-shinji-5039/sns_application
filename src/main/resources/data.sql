2086aa6b043d2adaa630dd63c22ddb1d39884c43
logical-natsu <natsuhara.shinji.5039@gmail.com>
2023-05-30 17:34:00 +0900

    設定

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO accounts(id, name, email, introduction, password, birthday, created_at, updated_at) VALUES('test_taro', 'taro', 'taro@taro', 'こんにちは', 'test_taro', '2000-11-11', '2023-04-15 20:15:30', '2023-04-15 20:15:30');
+INSERT INTO accounts(id, name, email, introduction, password, birthday, created_at, updated_at) VALUES('test_jiro', 'jiro', 'jiro@jiro', 'こんにちは', 'test_jiro', '2004-08-20', '2023-04-20 20:15:30', '2023-04-20 20:15:30');
+INSERT INTO accounts(id, name, email, introduction, password, birthday, created_at, updated_at) VALUES('test_saboro', 'saboro', 'saboro@saboro', 'サボロー', 'test_saboro', '2000-04-07', '2011-05-17 11:15:20', '2011-05-17 11:15:20');
+INSERT INTO accounts(id, name, email, introduction, password, birthday, created_at, updated_at) VALUES('test', 'test', 'test@test', 'test', 'test', '2000-04-07', '2011-05-17 11:15:20', '2011-05-17 11:15:20');
+
+INSERT INTO posts(user_id, body, created_at, updated_at) VALUES('test_saboro', 'サボロー', '2023-05-21 09:02:40', '2023-05-20 09:02:40');
+INSERT INTO posts(user_id, body, created_at, updated_at) VALUES('test_saboro', 'サボロー', '2023-05-20 09:02:40', '2023-05-20 09:02:40');
+INSERT INTO posts(user_id, body, created_at, updated_at) VALUES('test_taro', 'スマブラやろう！！', '2023-05-24 09:02:40', '2023-05-24 09:02:40');
+INSERT INTO posts(user_id, body, created_at, updated_at) VALUES('test_jiro', 'マリカーやろう！！', '2023-05-25 09:02:40', '2023-05-25 09:02:40');
+INSERT INTO posts(user_id, body, created_at, updated_at) VALUES('test_jiro', 'ゲームやろう', '2023-05-25 09:02:40', '2023-05-25 09:02:40');
